b13de1b220f6aafb7785c467cb9fdf6f039ad4f7
JoshR <joshuarizek@gmail.com>
2020-05-13 20:12:02 -0400

    mvp reached ready to deploy but will work on CSS more

--- app.js
+++ app.js
@@ -3,88 +3,117 @@ const domain = "https://www.moogleapi.com/"
 const monsterUrl = `https://www.moogleapi.com/api/v1/monsters`
 const heroURL = `https://www.moogleapi.com/api/v1/characters/search?name=Cloud%20Strife`
 
-let section = document.querySelector('.monster-section')
+let section = document.querySelector('#monsterDiv')
 let monsterName = document.querySelector('.monster-name')
 let japaneseName = document.querySelector('.japanese-name')
 let monsterImg = document.querySelector('.randomMonster')
 
-let rMonster = []
-
-// let monsterH = null
+let rMonsterHealth = 0
+let heroHealth = 2800
+let hero_hitpoints = document.querySelector('.hero-health')
+let monster_hitpoints = document.querySelector('.monster-health')
 
 async function getRandomMonster() {
 
     try {
-        let monsterData = await axios.get(monsterUrl)
-        let randomNumber = Math.floor(Math.random() * monsterData.data.length) + 1
-        let randomMonster = monsterData.data[randomNumber]
-        console.log(randomMonster)
-        renderMonster(randomMonster)
-        if (randomMonster.hitPoints === 0){
+            let monsterData = await axios.get(monsterUrl)
+            let randomNumber = Math.floor(Math.random() * monsterData.data.length) + 1
+            let randomMonster = monsterData.data[randomNumber]
+            console.log(randomMonster)
+            
+            if (randomMonster.hitPoints === 0){
 
-            rMonster.push(4000)
+            rMonsterHealth = 4000
 
-        }else {
-            rMonster.push(randomMonster.hitPoints)
-
+        }else{
+    
+            rMonsterHealth = randomMonster.hitPoints
         }
-        
-        // monsterH = randomMonster.hitPoints
-        console.log(rMonster)
-    } catch{
-        console.log('error')
-    }
+        renderMonster(randomMonster)
+            }catch{
+            console.log('error')
+        }
 }
 getRandomMonster()
 
 function higherDamage() {
 
-    
-
     let randomNumber = Math.floor(Math.random() * 21)
     console.log(randomNumber + ' higher damage attack')
     if (randomNumber > 11) {
-        let mHealth = rMonster -= randomNumber
-        alert(`Cloud does ${randomNumber} damage! monster health is now ${mHealth}`)
-    } else {
+        rMonsterHealth -= randomNumber
+        alert(`Cloud does ${randomNumber} damage! monster health is now ${rMonsterHealth}`)
+    }else {
         alert('Cloud\'s attack misses!')
     }
+    hero_hitpoints.innerHTML = (`HP: ${heroHealth}`)
+    monster_hitpoints.innerHTML = (`HP: ${rMonsterHealth}`)
 }
 
 
 function lowerDamage() {
     let randomNumber = Math.floor(Math.random() * 10)
+    let monsterRoll = Math.floor(Math.random() * 21)
+
     console.log(randomNumber + ' lower damage attack')
     if (randomNumber > 3) {
-        let mHealth = rMonster -=randomNumber
-        alert(`Cloud does ${randomNumber} damage!, monster health is now ${mHealth}`)
+        rMonsterHealth -= randomNumber
+        alert(`Cloud does ${randomNumber} damage!, monster health is now ${rMonsterHealth}`)
     } else {
         alert('Cloud\'s attack misses!')
     }
+
+if(monsterRoll > 17 ){
+    heroHealth -= 400
+    alert(`critical hit! monster does 400 damage! Cloud's health is now ${heroHealth}`)
+
+}else if(12 < monsterRoll < 16){
+
+    heroHealth -= 275
+
+    alert(`monster does 275 damage, Cloud's health is now ${heroHealth}`)
+
+}else if(11 > monsterRoll > 7){
+
+    heroHealth -= 100
+
+    alert(`monster does 275 damage, Cloud's health is now ${heroHealth}`)
 }
+else{
+    
+    alert('monster missed')
+}
+hero_hitpoints.innerHTML = (`HP: ${heroHealth}`)
+monster_hitpoints.innerHTML = (`HP: ${rMonsterHealth}`)
+}
+
+
 
 
 
 getHero()
 
+// 'https://www.mariowiki.com/images/b/b3/Cloud_SSBU.png'
+
 function renderHero(hero) {
     let heroName = document.createElement('h1')
-    let heroPic = document.createElement('img')
+    let heroPic = document.querySelector('img')
+    
+    hero_hitpoints.innerHTML = (`HP: ${heroHealth}`)
     heroName.innerHTML = hero.name
-    heroPic.setAttribute("src", 'https://www.mariowiki.com/images/b/b3/Cloud_SSBU.png')
+    heroPic.setAttribute("src", 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a67ac30c-fe21-4e97-8dad-ffa8b2670167/dbpjw5t-23ed5ea9-1d62-4990-8983-75f245a5632b.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvYTY3YWMzMGMtZmUyMS00ZTk3LThkYWQtZmZhOGIyNjcwMTY3XC9kYnBqdzV0LTIzZWQ1ZWE5LTFkNjItNDk5MC04OTgzLTc1ZjI0NWE1NjMyYi5naWYifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.1DTa1_fvKZIw6jzmAI4X__cpXJg42AViBsZsXWl4t1w')
     heroPic.style.width = '225px'
-    heroPic.style.height = '250px'
+    heroPic.style.height = '225px'
+
 
 
     let heroDiv = document.querySelector('.hero')
 
     heroDiv.append(heroName)
     heroDiv.append(heroPic)
-
+    heroDiv.append(hero_hitpoints)
 }
 
-
-let heroHealth = 2000
 
 
 
@@ -108,29 +137,66 @@ function renderMonster(monster) {
     let monsterJName = document.createElement('h2')
     let monsterPic = document.createElement('img')
 
+    monster_hitpoints.innerHTML = (`HP: ${rMonsterHealth}`)
     monsterName.innerHTML = monster.name
     monsterJName.innerHTML = monster.japaneseName
-    monsterPic.setAttribute("src", monster.picture)
-    monsterPic.style.width = '225px'
-    monsterPic.style.height = '250px'
+    
+    if( monster.name === 'Chocobo'){
+    
+    monsterPic.setAttribute("src", "redchocobo.gif")
+    monsterPic.style.width = '250px'
+    monsterPic.style.height = '275px'
+    
+    
+
+    } else if(monster.name === 'Behemoth'){
+
+        monsterPic.setAttribute("src",'behemoth.gif')
+        monsterPic.style.width = '500px'
+        monsterPic.style.height = '400px' 
+        monsterPic.style.objectPosition = '0px 30px'
+        monsterPic.style.right = '50px'
+        monsterPic.style.bottom = '25px'
+
+    }else if( monster.name === 'Antlion'){
+
+        monsterPic.setAttribute("src",'antlion.gif')
+        monsterPic.style.width = '275px'
+        monsterPic.style.height = '300px' 
+        monsterPic.style.bottom = '10px'
+        monsterPic.style.objectPosition = '0px -75px'
+      
+    
+    }else{
+
+        monsterPic.setAttribute("src",'greyChocobo.gif')
+        monsterPic.style.width = '250px'
+        monsterPic.style.height = '275px'  
+    
+
+    }
+
+    
 
 
-    let monsterSect = document.querySelector('.monster-section')
+    let monsterSect = document.querySelector('#monsterDiv')
 
     monsterSect.append(monsterName)
     monsterSect.append(monsterJName)
     monsterSect.append(monsterPic)
+    monsterSect.append(monster_hitpoints)
 
 
 }
 
 
-async function getHero() {
+async function getHero(){
     try {
         let heroData = await axios.get(heroURL)
         console.log(heroData.data[0].name)
         renderHero(heroData.data[0])
-    } catch{
+    }catch{
         console.log('error')
     }
 }
+
