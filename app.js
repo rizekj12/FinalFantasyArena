a0840ac49a674da51b87a3e233a828cd7bf2b0c7
JoshR <joshuarizek@gmail.com>
2020-05-14 10:34:48 -0400

    mvp done still adding post mvp and polishing

--- app.js
+++ app.js
@@ -9,83 +9,128 @@ let japaneseName = document.querySelector('.japanese-name')
 let monsterImg = document.querySelector('.randomMonster')
 
 let rMonsterHealth = 0
-let heroHealth = 2800
+let heroHealth = 5000
 let hero_hitpoints = document.querySelector('.hero-health')
 let monster_hitpoints = document.querySelector('.monster-health')
 
 async function getRandomMonster() {
 
     try {
-            let monsterData = await axios.get(monsterUrl)
-            let randomNumber = Math.floor(Math.random() * monsterData.data.length) + 1
-            let randomMonster = monsterData.data[randomNumber]
-            console.log(randomMonster)
-            
-            if (randomMonster.hitPoints === 0){
+        let monsterData = await axios.get(monsterUrl)
+        let randomNumber = Math.floor(Math.random() * monsterData.data.length) + 1
+        let randomMonster = monsterData.data[randomNumber]
+        console.log(randomMonster)
+
+        if (randomMonster.hitPoints === 0) {
 
             rMonsterHealth = 4000
 
-        }else{
-    
+        } else {
+
             rMonsterHealth = randomMonster.hitPoints
         }
         renderMonster(randomMonster)
-            }catch{
-            console.log('error')
-        }
+    } catch{
+        console.log('error')
+    }
 }
 getRandomMonster()
 
 function higherDamage() {
-
     let randomNumber = Math.floor(Math.random() * 21)
-    console.log(randomNumber + ' higher damage attack')
-    if (randomNumber > 11) {
-        rMonsterHealth -= randomNumber
-        alert(`Cloud does ${randomNumber} damage! monster health is now ${rMonsterHealth}`)
-    }else {
+    let monsterRoll = Math.floor(Math.random() * 21)
+
+    if (randomNumber >= 17) {
+        rMonsterHealth -= 700
+        alert(`CRITICAL HIT!! Cloud does 700 damage!`)
+    } else if (17 > randomNumber >= 12) {
+        rMonsterHealth -= 400
+        alert(`Cloud does 400 damage!`)
+    } else if (12 > randomNumber >= 10) {
+        rMonsterHealth -= 300
+        alert('Cloud does 300 damage!')
+    } else if (randomNumber < 9) {
         alert('Cloud\'s attack misses!')
+    } else {
+        rMonsterHealth -= 100
+        alert('Cloud does 100 damage')
     }
+
+    if (monsterRoll >= 17) {
+        heroHealth -= 400
+        alert(`critical hit! monster does 400 damage!`)
+    } else if (12 <= monsterRoll < 17) {
+        heroHealth -= 225
+        alert(`monster does 225 damage`)
+    } else if (12 > monsterRoll >= 8) {
+        heroHealth -= 100
+        alert(`monster does 275 damage`)
+    }
+    else {
+        alert('monster missed')
+    }
+
+
     hero_hitpoints.innerHTML = (`HP: ${heroHealth}`)
     monster_hitpoints.innerHTML = (`HP: ${rMonsterHealth}`)
+
+    if (heroHealth === 0) {
+
+        alert('you have lost, Game Over! :(')
+    } else if (rMonsterHealth === 0) {
+
+        alert('Cloud wins the battle!!')
+    }
 }
+
 
 
 function lowerDamage() {
-    let randomNumber = Math.floor(Math.random() * 10)
+    let randomNumber = Math.floor(Math.random() * 21)
     let monsterRoll = Math.floor(Math.random() * 21)
 
-    console.log(randomNumber + ' lower damage attack')
-    if (randomNumber > 3) {
-        rMonsterHealth -= randomNumber
-        alert(`Cloud does ${randomNumber} damage!, monster health is now ${rMonsterHealth}`)
-    } else {
+    if (randomNumber >= 15) {
+        rMonsterHealth -= 300
+        alert(`Cloud does 300 damage!`)
+    } else if (15 > randomNumber >= 9) {
+        rMonsterHealth -= 250
+        alert(`Cloud does 250 damage!`)
+    } else if (9 > randomNumber >= 3) {
+        rMonsterHealth -= 200
+        alert('Cloud does 250 damage!')
+    } else if (3 > randomNumber) {
         alert('Cloud\'s attack misses!')
+    } else{
+        rMonsterHealth -= 150
+        alert('Cloud does 150 damage')
     }
 
-if(monsterRoll > 17 ){
-    heroHealth -= 400
-    alert(`critical hit! monster does 400 damage! Cloud's health is now ${heroHealth}`)
+    if (monsterRoll >= 17) {
+        heroHealth -= 350
+        alert(`critical hit! monster does 350 damage!`)
+    } else if (12 <= monsterRoll < 17) {
+        heroHealth -= 275
+        alert(`monster does 275 damage`)
+    } else if (17 >= monsterRoll > 7) {
+        heroHealth -= 100
+        alert(`monster does 275 damage,`)
+    } else if (monsterRoll < 7)(
+        alert('monster missed')
+    )
+    else {
+        alert('monster missed')
+    }
 
-}else if(12 < monsterRoll < 16){
+    hero_hitpoints.innerHTML = (`Health: ${heroHealth}`)
+    monster_hitpoints.innerHTML = (`Health: ${rMonsterHealth}`)
 
-    heroHealth -= 275
-
-    alert(`monster does 275 damage, Cloud's health is now ${heroHealth}`)
-
-}else if(11 > monsterRoll > 7){
-
-    heroHealth -= 100
-
-    alert(`monster does 275 damage, Cloud's health is now ${heroHealth}`)
+    if (heroHealth <= 0) {
+        alert('you have lost, Game Over! :(')
+    } else if (rMonsterHealth <= 0) {
+        alert('CLoud wins the battle!!')
+    }
 }
-else{
-    
-    alert('monster missed')
-}
-hero_hitpoints.innerHTML = (`HP: ${heroHealth}`)
-monster_hitpoints.innerHTML = (`HP: ${rMonsterHealth}`)
-}
+
 
 
 
@@ -98,7 +143,7 @@ getHero()
 function renderHero(hero) {
     let heroName = document.createElement('h1')
     let heroPic = document.querySelector('img')
-    
+
     hero_hitpoints.innerHTML = (`HP: ${heroHealth}`)
     heroName.innerHTML = hero.name
     heroPic.setAttribute("src", 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a67ac30c-fe21-4e97-8dad-ffa8b2670167/dbpjw5t-23ed5ea9-1d62-4990-8983-75f245a5632b.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvYTY3YWMzMGMtZmUyMS00ZTk3LThkYWQtZmZhOGIyNjcwMTY3XC9kYnBqdzV0LTIzZWQ1ZWE5LTFkNjItNDk5MC04OTgzLTc1ZjI0NWE1NjMyYi5naWYifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.1DTa1_fvKZIw6jzmAI4X__cpXJg42AViBsZsXWl4t1w')
@@ -127,7 +172,6 @@ let move2 = document.querySelector('.move2')
 
 
 move1.addEventListener('click', higherDamage)
-
 move2.addEventListener('click', lowerDamage)
 
 
@@ -140,43 +184,43 @@ function renderMonster(monster) {
     monster_hitpoints.innerHTML = (`HP: ${rMonsterHealth}`)
     monsterName.innerHTML = monster.name
     monsterJName.innerHTML = monster.japaneseName
-    
-    if( monster.name === 'Chocobo'){
-    
-    monsterPic.setAttribute("src", "redchocobo.gif")
-    monsterPic.style.width = '250px'
-    monsterPic.style.height = '275px'
-    
-    
 
-    } else if(monster.name === 'Behemoth'){
+    if (monster.name === 'Chocobo') {
 
-        monsterPic.setAttribute("src",'behemoth.gif')
+        monsterPic.setAttribute("src", "redchocobo.gif")
+        monsterPic.style.width = '250px'
+        monsterPic.style.height = '275px'
+
+
+
+    } else if (monster.name === 'Behemoth') {
+
+        monsterPic.setAttribute("src", 'behemoth.gif')
         monsterPic.style.width = '500px'
-        monsterPic.style.height = '400px' 
-        monsterPic.style.objectPosition = '0px 30px'
+        monsterPic.style.height = '400px'
+        monsterPic.style.objectPosition = '-10px 60px'
         monsterPic.style.right = '50px'
         monsterPic.style.bottom = '25px'
 
-    }else if( monster.name === 'Antlion'){
+    } else if (monster.name === 'Antlion') {
 
-        monsterPic.setAttribute("src",'antlion.gif')
-        monsterPic.style.width = '275px'
-        monsterPic.style.height = '300px' 
+        monsterPic.setAttribute("src", 'antlion.gif')
+        monsterPic.style.width = '375px'
+        monsterPic.style.height = '425px'
         monsterPic.style.bottom = '10px'
-        monsterPic.style.objectPosition = '0px -75px'
-      
-    
-    }else{
+        monsterPic.style.objectPosition = '-10px 125px'
 
-        monsterPic.setAttribute("src",'greyChocobo.gif')
+
+    } else {
+
+        monsterPic.setAttribute("src", 'greyChocobo.gif')
         monsterPic.style.width = '250px'
-        monsterPic.style.height = '275px'  
-    
+        monsterPic.style.height = '275px'
+
 
     }
 
-    
+
 
 
     let monsterSect = document.querySelector('#monsterDiv')
@@ -190,12 +234,12 @@ function renderMonster(monster) {
 }
 
 
-async function getHero(){
+async function getHero() {
     try {
         let heroData = await axios.get(heroURL)
         console.log(heroData.data[0].name)
         renderHero(heroData.data[0])
-    }catch{
+    } catch{
         console.log('error')
     }
 }
